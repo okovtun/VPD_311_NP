e2243205d0bec4d9e756133b22f51cf90680f070
User <User@DESKTOP-QHG18FL>
2025-06-15 13:14:38 +0300

    WinSockClient start

---
+++ NetworkProgramming/WinSockClient/main.cpp
@@ -0,0 +1,60 @@
+﻿#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // !WIN32_LEAN_AND_MEAN
+
+#include<Windows.h>
+#include<WinSock2.h>
+#include<WS2tcpip.h>
+#include<iphlpapi.h>
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#define DEFAULT_PORT	"27015"
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "WinSock Client" << endl;
+	//1) Инициализация WinSock:
+	WSADATA wsaData;
+	INT iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult)
+	{
+		cout << "WSAStartup() failed with code " << iResult << endl;
+		return;
+	}
+
+	//2) Создаем ClientSocket:
+	addrinfo* result = NULL;
+	addrinfo hints;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;	//INET - TCP/IPv4
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	//3) Определяем IP-адрес Сервера:
+	iResult = getaddrinfo("127.0.0.1", DEFAULT_PORT, &hints, &result);
+	if (iResult)
+	{
+		cout << "getaddressinfo() failed with code " << iResult << endl;
+		WSACleanup();
+		return;
+	}
+	cout << "hints:" << endl;
+	cout << "ai_addr:" << hints.ai_addr->sa_data << endl;
+
+	SOCKET connect_socket = socket(hints.ai_family, hints.ai_socktype, hints.ai_protocol);
+	if (connect_socket == INVALID_SOCKET)
+	{
+		cout << "Error: soket creatin failed with code: " << WSAGetLastError();
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	//?) Освобождаем ресурсы WinSock:
+	WSACleanup();
+}
